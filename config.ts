d71e19c259207073f1a7d1f3013bf831d5c4f380
tarek-gritli <tarek.gritli@insat.ucar.tn>
2024-08-28 11:05:58 +0100

    Update config.ts and add necessary libraries

--- config.ts
+++ config.ts
@@ -1,3 +1,4 @@
+import { S3Client } from "@aws-sdk/client-s3";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -5,8 +6,19 @@ export const NODE_ENV = process.env.NODE_ENV || "development";
 export const PORT =
   NODE_ENV === "production" ? 8080 : parseInt(process.env.PORT || "3000", 10);
 export const FILE_SERVER_URL =
-  process.env.FILE_SERVER_URL || "http://localhost:3000";
+  NODE_ENV === "production"
+    ? process.env.FILE_SERVER_URL
+    : "http://localhost:3000";
 export const FILEBASE_BUCKET = process.env.FILEBASE_BUCKET || "";
-export const FILEBASE_ACCESS_KEY = process.env.FILEBASE_ACCESS_KEY || "";
-export const FILEBASE_SECRET_KEY = process.env.FILEBASE_SECRET_KEY || "";
-export const FILEBASE_REGION = process.env.FILEBASE_REGION || "";
+const FILEBASE_ACCESS_KEY = process.env.FILEBASE_ACCESS_KEY || "";
+const FILEBASE_SECRET_KEY = process.env.FILEBASE_SECRET_KEY || "";
+const FILEBASE_REGION = process.env.FILEBASE_REGION || "";
+
+export const s3 = new S3Client({
+  region: FILEBASE_REGION,
+  endpoint: "https://s3.filebase.com",
+  credentials: {
+    accessKeyId: FILEBASE_ACCESS_KEY,
+    secretAccessKey: FILEBASE_SECRET_KEY,
+  },
+});
